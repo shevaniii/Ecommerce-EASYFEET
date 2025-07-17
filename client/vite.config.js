52e8644665881fe0a0deb8fa56da4b81a566dc71
Shivani giri <Shivanigiri36@gmail.com>
2025-07-17 15:32:41 +0530

    updating link in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   server:{
     proxy:{
     '/api' :{
-     target:'http://localhost:3000',
+     target:"https://easyfeet-shoes.onrender.com",
      changeOrigin:true,
      secure:false
     }
