f6502636406cea8a518eee60d9569a0d303f98e9
Shivani giri <Shivanigiri36@gmail.com>
2025-07-18 23:02:59 +0530

    making chnges in index.js

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ import orderRoute from './routes/orderRoute.js'
 
 connectDB()
 const app = express()
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ecommerce-easyfeet.vercel.app", 
-    ],
-     methods: 'GET,HEAD,PUT,POST,DELETE',
-    credentials: true,
-    optionSuccessStatus:200
-  })
-);
-app.use((req, res, next) => {
-          res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-easyfeet-shevaniiis-projects.vercel.app');
-          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-          res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-          next();
-        });
+ // Enable CORS for all routes or specific routes
+    app.use(cors({
+      origin: 'https://ecommerce-easyfeet-shevaniiis-projects.vercel.app',
+      methods: ['GET','POST','PUT','DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true // If you need to send cookies
+    }));
+// app.use((req, res, next) => {
+//           res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-easyfeet-shevaniiis-projects.vercel.app');
+//           res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//           res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//           next();
+//         });
 app.use(express.json()) //to parse data into json 
 app.use('/api/products', productRoute)
 app.use('/api/auth', authRoute)   // all authentication routes should have prefix '/auth' .
